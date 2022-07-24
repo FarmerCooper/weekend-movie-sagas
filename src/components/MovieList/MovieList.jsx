4ae3623cc8c9b6f34e9e4b06ea6b597c62ece3df
Diego Baeza <diegob12@live.com>
2022-07-24 15:37:46 -0500

    imported react-bootstrap, add CARDS to MovieList page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,42 +1,55 @@
-import React, { useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {useHistory} from 'react-router-dom'
-import './MovieList.css'
+import React, { useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import "./MovieList.css";
 
 function MovieList() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const movies = useSelector((store) => store.movies);
 
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const movies = useSelector(store => store.movies);
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" });
+  }, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES'});
-    }, []);
+  const imgClick = (movie) => {
+    console.log("This image was clicked", movie.id);
+    dispatch({ type: "IMG_CLICKED", payload: movie.id });
 
-    const imgClick = (movie) => {
+    setTimeout(() => {
+      history.push("/details");
+    }, 25);
+  };
 
-        console.log('This image was clicked', movie.id);
-        dispatch({type: 'IMG_CLICKED', payload: movie.id});
-
-        setTimeout(() => {history.push("/details")}, 25)
-    }
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img onClick={event => imgClick(movie)} src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+  return (
+    <main>
+      <h1>MovieList</h1>
+      <section className="movies">
+        {movies.map((movie) => {
+          return (
+            <div key={movie.id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src={movie.poster}
+                  onClick={(event) => imgClick(movie)}
+                  alt={movie.title}
+                />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default MovieList;
