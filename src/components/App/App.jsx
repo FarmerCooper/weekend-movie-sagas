47aa776e303064eaa58edd5e3b784c3fdbc6e69e
Diego Baeza <diegob12@live.com>
2022-07-24 16:15:18 -0500

    add route of homepage to app title only

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,44 +1,40 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import { useDispatch } from "react-redux";
 
 // Components will go here
-import MovieList from '../MovieList/MovieList'
-import DetailsView from '../DetailsView/DetailsView';
-
+import MovieList from "../MovieList/MovieList";
+import DetailsView from "../DetailsView/DetailsView";
 
 function App() {
-
   const dispatch = useDispatch();
 
-    // Add dispatch to for SAGA to listen
-    // User sent to DetailsView page
-    // Gets Details
-    const sendToDetailsView = () => {
-      dispatch({type: 'GET_DETAILS'})
-    }
-
+  // Add dispatch to for SAGA to listen
+  // User sent to DetailsView page
+  // Gets Details
+  const sendToDetailsView = () => {
+    dispatch({ type: "GET_DETAILS" });
+  };
 
   return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
+    <Router>
+      <div className="App">
+        <Route path="/" exact>
+          <h1>The Movies Saga!</h1>
+        </Route>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
         <Router>
           <Route path="/details" exact>
             <DetailsView />
           </Route>
         </Router>
-
-        {/* Add Movie page */}
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
-
 
 export default App;
